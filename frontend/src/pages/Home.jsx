0f7298d4b04a42ec5794a2cce000ae7326913a47
Gustavo-Nog <gustavopan3g7@gmail.com>
2025-07-01 19:23:38 -0300

    adiciona a sub-pasta 'pages' e os arquivos de 'Login.jsx', 'Cadastro.jsx' e 'Home.jsx', para a estruturação inicial

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+function Home() {
+  return (
+    <>
+      <div>
+        <h1>Bem vindo a pagina principal</h1>
+        <p>
+          Sistema para Feira soliddaria, desenvolvidos por alunos do programa Capacita Brasil.
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default Home;
