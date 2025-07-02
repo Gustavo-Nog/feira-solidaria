8350d3d51da37bc81112c0c1d91052fab265e639
GABRIEL SILVA CRISPIM <gabrielcrispim506@gmail.com>
2025-07-02 17:34:27 -0300

    Texto no login

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,14 @@
+function Login() {
+  return (
+    <>
+      <div>
+        <h1>Página de Login</h1>
+        <p>
+          Sistema para Feira solidaria, desenvolvidos por alunos do programa Capacita Brasil.
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default Login;
