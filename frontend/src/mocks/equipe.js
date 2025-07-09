8dbe39dcfe64ec03b1e18a4b6eb71d6b0066f0c3
Gustavo-Nog <gustavopan3g7@gmail.com>
2025-07-09 13:26:43 -0300

    Corrige nome incorreto de membro na lista de equipe

--- frontend/src/mocks/equipe.js
+++ frontend/src/mocks/equipe.js
@@ -3,7 +3,7 @@ const membrosEquipe = [
   { nome: 'Gustavo', nacionalidade: 'Brasileiro', github: 'https://github.com/Gustavo-Nog', foto: "/assets/equipe/gustavo.jpg" },
   { nome: 'Henrique', nacionalidade: 'Brasileiro', github: 'https://github.com/HenriqueAdriel', foto: "/assets/equipe/henrique.jpg" },
   { nome: 'Hudson', nacionalidade: 'Brasileiro', github: 'https://github.com/Hudson2207', foto: "/assets/equipe/hudson.jpg" },
-  { nome: 'Marianne', nacionalidade: 'Brasileira', github: 'https://github.com/Marionofree', foto: "/assets/equipe/marianne.jpg" }, ,
+  { nome: 'Marianna', nacionalidade: 'Brasileira', github: 'https://github.com/Marionofree', foto: "/assets/equipe/marianne.jpg" }, ,
   { nome: 'Thierry', nacionalidade: 'Brasileiro', github: 'https://github.com/Thierry-DV', foto: "/assets/equipe/thierry.jpg" },
 ];
 
