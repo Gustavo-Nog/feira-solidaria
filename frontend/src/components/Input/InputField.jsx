c5857417277b94e1dfb0d76c75eaa009d32c0ee2
Gustavo-Nog <gustavopan3g7@gmail.com>
2025-07-12 20:07:43 -0300

    style: permite customização de classe via props no componente InputField

--- frontend/src/components/Input/InputField.jsx
+++ frontend/src/components/Input/InputField.jsx
@@ -1,7 +1,7 @@
 import './InputField.css';
 import { useFormContext } from 'react-hook-form';
 
-function InputField({ name, label, type = 'text', required }) {
+function InputField({ name, label, type = 'text', required, className = '' }) {
   const {
     register,
     formState: { errors },
@@ -9,14 +9,13 @@ function InputField({ name, label, type = 'text', required }) {
 
   return (
     <div className="input-wrapper">
-      <label htmlFor={name}>{label}</label>
+      <label htmlFor={name} className="form-label">{label}</label>
       <input
         id={name}
         type={type}
-        className="custom-input"
+        className={`custom-input ${className}`} // junta a sua classe + bootstrap
         {...register(name, { required })}
         placeholder={`Digite seu ${label.toLowerCase()}`}
-        autoComplete="off"
       />
       {errors[name] && (
         <span className="text-danger">{errors[name].message}</span>
