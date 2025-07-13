cdfafc933410ecd0f7e4f23ab68eaee2bae4ae8d
Gustavo-Nog <gustavopan3g7@gmail.com>
2025-07-13 16:32:47 -0300

    refactor: Corrige dados de mock da equipe e ajusta caminho das imagens

--- frontend/src/mocks/equipe.js
+++ frontend/src/mocks/equipe.js
@@ -1,10 +1,22 @@
+{/* coloquem uma imagem de vocês com o nome do aquivo correspondente */ }
+//  import gabrielFoto from "../assets/equipe/gabriel.jpg";
+import gustavoFoto from "../assets/equipe/gustavo.png";
+//  import henriqueFoto from "../assets/equipe/henrique.jpg";
+// import hudsonFoto from "../assets/equipe/hudson.jpg";
+//  import mariannaFoto from "../assets/equipe/marianne.jpg";
+//  import thierryFoto from "../assets/equipe/thierry.jpg";
+
+const placeholder = "https://via.placeholder.com/150";
+
+{/* Lembrem-se de tirar o placeholder da 'foto: placeholder' e substituam de acordo nome correspondente */ }
+
 const membrosEquipe = [
-  { nome: 'Gabriel', nacionalidade: 'Brasileiro', github: 'https://github.com/gabrielcrispim-c', foto: "/assets/equipe/gabriel.jpg" },
-  { nome: 'Gustavo', nacionalidade: 'Brasileiro', github: 'https://github.com/Gustavo-Nog', foto: "/assets/equipe/gustavo.jpg" },
-  { nome: 'Henrique', nacionalidade: 'Brasileiro', github: 'https://github.com/HenriqueAdriel', foto: "/assets/equipe/henrique.jpg" },
-  { nome: 'Hudson', nacionalidade: 'Brasileiro', github: 'https://github.com/Hudson2207', foto: "/assets/equipe/hudson.jpg" },
-  { nome: 'Marianna', nacionalidade: 'Brasileira', github: 'https://github.com/Marionofree', foto: "/assets/equipe/marianne.jpg" }, ,
-  { nome: 'Thierry', nacionalidade: 'Brasileiro', github: 'https://github.com/Thierry-DV', foto: "/assets/equipe/thierry.jpg" },
+  { nome: 'Gabriel', nacionalidade: 'Brasileiro', github: 'https://github.com/gabrielcrispim-c', foto: placeholder },
+  { nome: 'Gustavo', nacionalidade: 'Brasileiro', github: 'https://github.com/Gustavo-Nog', foto: gustavoFoto },
+  { nome: 'Henrique', nacionalidade: 'Brasileiro', github: 'https://github.com/HenriqueAdriel', foto: placeholder },
+  { nome: 'Hudson', nacionalidade: 'Brasileiro', github: 'https://github.com/Hudson2207', foto: placeholder },
+  { nome: 'Marianna', nacionalidade: 'Brasileira', github: 'https://github.com/Marionofree', foto: placeholder },
+  { nome: 'Thierry', nacionalidade: 'Brasileiro', github: 'https://github.com/Thierry-DV', foto: placeholder },
 ];
 
 export default membrosEquipe;
