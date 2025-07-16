857a4a519b442b65912be6b248cb323fd3847818
Thierry <144447969+Thierry-DV@users.noreply.github.com>
2025-07-16 08:17:22 -0300

    Update ItemDetalhe.jsx

--- frontend/src/pages/ItemDetalhe/ItemDetalhe.jsx
+++ frontend/src/pages/ItemDetalhe/ItemDetalhe.jsx
@@ -3,13 +3,11 @@ import { useParams, useNavigate } from 'react-router-dom'; // 1. Importe o useNa
 import { mockItem } from '../../mocks/itens';
 import './ItemDetalhe.css';
 
-// --- Simulação de Autenticação e Carrinho ---
-// No seu projeto real, estes valores e funções viriam de um Contexto ou Redux.
 const usuarioLogado = false; // Mude para 'false' para testar o redirecionamento para o login.
 
 const adicionarAoCarrinho = (item) => {
   console.log(`Item "${item.nome}" adicionado ao carrinho!`);
-  // Aqui você adicionaria a lógica real para atualizar o estado do carrinho.
+  // Logica do carrinho.
 };
 // ---------------------------------------------
 
