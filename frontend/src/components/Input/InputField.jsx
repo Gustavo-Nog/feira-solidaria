187b37bfbb99b24f3a5a3ec26684ec8d0a8e6b83
Marianna Onofre <nisousa.mari23@gmail.com>
2025-07-16 17:03:33 -0300

    adicionado componente InputField na tela de cadastro de item

--- frontend/src/components/Input/InputField.jsx
+++ frontend/src/components/Input/InputField.jsx
@@ -1,22 +1,58 @@
 import './InputField.css';
 import { useFormContext } from 'react-hook-form';
 
-function InputField({ name, label, type = 'text', required, className = '' }) {
+
+function InputField({ name, label, type = 'text', required, className = '', as = 'input', options = [] }) {
   const {
     register,
     formState: { errors },
   } = useFormContext();
 
+  const renderField = () => {
+
+    switch (as) {
+      case 'textarea':
+        return (
+          <textarea
+            id={name}
+            className={`custom-input ${className}`}
+            {...register(name, { required })}
+            placeholder={`Digite a ${label.toLowerCase()}`}
+            rows="3"
+          />
+        );
+      case 'select':
+        return (
+          <select
+            id={name}
+            className={`custom-input ${className}`}
+            {...register(name, { required })}
+          >
+            <option value="">Selecione</option>
+            {options.map((option) => (
+              <option key={option.value} value={option.value}>
+                {option.label}
+              </option>
+            ))}
+          </select>
+        );
+      default:
+        return (
+          <input
+            id={name}
+            type={type}
+            className={`custom-input ${className}`}
+            {...register(name, { required })}
+            placeholder={`Digite seu ${label.toLowerCase()}`}
+          />
+        );
+    }
+  };
+
   return (
-    <div className="input-wrapper">
+    <div className="input-wrapper campo">
       <label htmlFor={name} className="form-label">{label}</label>
-      <input
-        id={name}
-        type={type}
-        className={`custom-input ${className}`} // junta a sua classe + bootstrap
-        {...register(name, { required })}
-        placeholder={`Digite seu ${label.toLowerCase()}`}
-      />
+      {renderField()} 
       {errors[name] && (
         <span className="text-danger">{errors[name].message}</span>
       )}
