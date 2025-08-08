be39ffb42ace998c0fbcbb4e09596cff81b99996
Gustavo-Nog <gustavopan3g7@gmail.com>
2025-08-07 22:09:38 -0300

    feat: adiciona pasta src no projeto com a estrutura MVC adiciona os arquivos para instanciamento das rotas

---
+++ backend/src/server.js
@@ -0,0 +1,9 @@
+import app from './app';
+require('dotenv').config();
+
+const app = require('./app');
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
