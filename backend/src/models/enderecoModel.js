813f4289e0d448666cf4f6a4bdcbeb485407faa2
Thierry-DV <thierryuchoa7@gmail.com>
2025-08-23 14:29:00 -0300

    feat: adicionar model e operações CRUD para endereços

---
+++ backend/src/models/enderecoModel.js
@@ -0,0 +1,72 @@
+const prisma = require('../prisma');
+
+const listarEnderecos = async () => {
+  return prisma.endereco.findMany();
+};
+
+const buscarEnderecoPorId = async (id) => {
+  return prisma.endereco.findUnique({
+    where: {
+      id
+    }
+  });
+};
+
+const criarEndereco = async (dadosEndereco) => {
+  const enderecoExistente = await prisma.endereco.findFirst({
+    where: {
+      cep: dadosEndereco.cep,
+      logradouro: dadosEndereco.logradouro,
+      numeroResidencia: dadosEndereco.numeroResidencia,
+    }
+  });
+
+  if (enderecoExistente) {
+    return enderecoExistente;
+  }
+
+  return prisma.endereco.create({
+    data: dadosEndereco
+  });
+};
+
+const atualizarEndereco = async (id, dadosParaAtualizar) => {
+  const enderecoExistente = await prisma.endereco.findUnique({
+    where: { id },
+  });
+
+  if (!enderecoExistente) {
+    throw new Error("Endereço não encontrado!");
+  }
+
+  return prisma.endereco.update({
+    where: {
+      id
+    },
+    data: dadosParaAtualizar
+  });
+};
+
+const deletarEndereco = async (id) => {
+  const enderecoExistente = await prisma.endereco.findUnique({
+    where: { id },
+  });
+
+  if (!enderecoExistente) {
+    throw new Error("Endereço não encontrado!");
+  }
+
+  return prisma.endereco.delete({
+    where: {
+      id
+    },
+  });
+};
+
+module.exports = {
+  listarEnderecos,
+  buscarEnderecoPorId,
+  criarEndereco,
+  atualizarEndereco,
+  deletarEndereco
+};
