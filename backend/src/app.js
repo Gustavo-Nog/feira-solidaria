549e8e224198745accb0ccda65806ef101222b17
Thierry-DV <thierryuchoa7@gmail.com>
2025-08-23 14:30:00 -0300

    refactor: reorganizar rotas de pessoas e endereços no app

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+const pessoaRoutes = require('./routes/pessoaRoutes');
+const enderecoRoutes = require('./routes/enderecoRoutes');
+
+app.use('/api/enderecos', enderecoRoutes);
+app.use('/api/pessoas', pessoaRoutes);
+
 app.get('/', (req, res) => {
     res.send("feira-solidaria");
 });
