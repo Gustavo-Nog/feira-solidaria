f55be586f0957fa69976bbff16879c6be63f8da8
hudson <hudsonlevi0007@gmail.com>
2025-08-27 22:25:45 -0300

    colocando as rotas de produto e favorito

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const produtoRoutes = require('./routes/produtoRoutes');
+const favoritoRoutes = require('./routes/favoritoRoutes');
+
+app.use('/produtos', produtoRoutes); 
+app.use('/favoritos', favoritoRoutes);
 
 app.use(express.json());
 
---
+++ backend/src/models/favorotoModel.js
@@ -0,0 +1,53 @@
+const prisma = require('../generated/prisma');
+
+const listarFavoritos = async () => {
+  return prisma.favorito.findMany({
+    include: {
+      pessoa: true,
+      produto: true
+    },
+    orderBy: {
+      dataAdicao: "desc"
+    }
+  });
+};
+
+const buscarFavoritoPorId = async (id) => {
+  return prisma.favorito.findUnique({
+    where: { id },
+    include: {
+      pessoa: true,
+      produto: true
+    }
+  });
+};
+
+const criarFavorito = async (dadosFavorito) => {
+  if (!dadosFavorito.pessoaId || !dadosFavorito.produtoId) {
+    throw new Error("PessoaId e ProdutoId são obrigatórios para criar um favorito.");
+  }
+  return prisma.favorito.create({
+    data: dadosFavorito,
+    include: {
+      pessoa: true,
+      produto: true
+    }
+  });
+};
+
+const deletarFavorito = async (id) => {
+  const favoritoExistente = await prisma.favorito.findUnique({ where: { id } });
+
+  if (!favoritoExistente) {
+    throw new Error("Favorito não encontrado!");
+  }
+
+  return prisma.favorito.delete({ where: { id } });
+};
+
+module.exports = {
+    listarFavoritos,
+    buscarFavoritoPorId,
+    criarFavorito,
+    deletarFavorito
+};
