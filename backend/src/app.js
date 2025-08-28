abe03f2551ccfc98e6abe826eebfe6efbb6be8ee
hudson <hudsonlevi0007@gmail.com>
2025-08-28 19:48:12 -0300

    adicionando const favorito e const produto na app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const app = express();
-const produtoRoutes = require('./routes/produtoRoutes');
-const favoritoRoutes = require('./routes/favoritoRoutes');
 
-app.use('/produtos', produtoRoutes); 
-app.use('/favoritos', favoritoRoutes);
+
+
+
 
 app.use(express.json());
 
 const usuarioRoutes = require('./routes/usuarioRoutes');
 const pessoaRoutes = require('./routes/pessoaRoutes');
 const enderecoRoutes = require('./routes/enderecoRoutes');
+const favoritoRoutes = require('./routes/favoritoRoutes');
+const produtoRoutes = require('./routes/produtoRoutes');
 
 app.use('/api/usuarios', usuarioRoutes);
 app.use('/api/enderecos', enderecoRoutes);
--- backend/src/routes/produtoRoutes.js
+++ backend/src/routes/produtoRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+
 const produtoController = require('../controllers/produtoController');
 
 router.get('/', produtoController.listarProdutosHandler);
