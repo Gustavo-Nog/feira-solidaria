2f164bb96c500c42534c2907b1b8fb7337568018
Gustavo-Nog <gustavopan3g7@gmail.com>
2025-08-29 18:16:10 -0300

    feat: chama o authRoutes no arquivo

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,16 +3,23 @@ const app = express();
 
 app.use(express.json());
 
+<<<<<<< HEAD
 const pessoaRoutes = require('./routes/pessoaRoutes');
 const acoesAdmRoutes = require('./routes/acoesAdmRoutes');
 const categoriaRoutes = require('./routes/categoriaRoutes');
 const usuarioRoutes = require('./routes/usuarioRoutes');
+=======
+const usuarioRoutes = require('./routes/usuarioRoutes');
+const authRoutes = require('./routes/authRoutes');
+const pessoasRoutes = require('./routes/pessoaRoutes');
+>>>>>>> ad6531a (feat: chama o authRoutes no arquivo)
 const enderecoRoutes = require('./routes/enderecoRoutes');
 const telefoneRoutes = require('./routes/telefoneRoutes');
 
 app.use('/api/acoesAdm', acoesAdmRoutes);
 app.use('/api/categorias', categoriaRoutes);
 app.use('/api/usuarios', usuarioRoutes);
+app.use('/api/auth', authRoutes);
 app.use('/api/enderecos', enderecoRoutes);
 app.use('/api/pessoas', pessoaRoutes);
 app.use('/api/telefones', telefoneRoutes);
