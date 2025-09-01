85ca071ae2abb1e7e6c08afbd33ea6c8aa4a6494
GABRIEL SILVA CRISPIM <gabrielcrispim506@gmail.com>
2025-09-01 14:08:01 -0300

    correção de variável na 'app.js'

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,23 +1,19 @@
 const express = require('express');
-const app = (express.json());
+const app = express();
+
+app.use(express.json());
 
 const pessoaRoutes = require('./routes/pessoaRoutes');
 const acoesAdmRoutes = require('./routes/acoesAdmRoutes');
 const categoriaRoutes = require('./routes/categoriaRoutes');
+const usuarioRoutes = require('./routes/usuarioRoutes');
+const enderecoRoutes = require('./routes/enderecoRoutes');
 
 app.use('/api/pessoas', pessoaRoutes);
 app.use('/api/acoesAdm', acoesAdmRoutes);
 app.use('/api/categorias', categoriaRoutes);
-
-app.use(express.json());
-
-const usuarioRoutes = require('./routes/usuarioRoutes');
-const pessoaRoutes = require('./routes/pessoaRoutes');
-const enderecoRoutes = require('./routes/enderecoRoutes');
-
 app.use('/api/usuarios', usuarioRoutes);
 app.use('/api/enderecos', enderecoRoutes);
-app.use('/api/pessoas', pessoaRoutes);
 
 app.get('/', (req, res) => {
     res.send("feira-solidaria");
