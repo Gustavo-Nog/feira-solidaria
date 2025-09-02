dcd1d41a462e84425c88aa102b0bf5f0b22acfab
Thierry-DV <thierryuchoa7@gmail.com>
2025-09-02 00:03:52 -0300

    feat: adicionar gerenciamento de mensagens com rotas, controladores e modelos

---
+++ backend/src/controllers/mensagemController.js
@@ -0,0 +1,57 @@
+const mensagensModel = require('../models/mensagemModel');
+
+const listarMensagens = async (req, res) => {
+  try {
+    const mensagens = await mensagemModel.listarMensagens();
+    res.status(200).json(mensagens);
+  } catch (error) {
+    res.status(500).json({ erro: error.message });
+  }
+};
+
+const buscarMensagemPorId = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const mensagem = await mensagemModel.buscarMensagemPorId(Number(id));
+    res.status(200).json(mensagem);
+  } catch (error) {
+    res.status(404).json({ erro: error.message });
+  }
+};
+
+const criarMensagem = async (req, res) => {
+  try {
+    const novaMensagem = await mensagemModel.criarMensagem(req.body);
+    res.status(201).json(novaMensagem);
+  } catch (error) {
+    res.status(400).json({ erro: error.message });
+  }
+};
+
+const atualizarMensagem = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const mensagemAtualizada = await mensagemModel.atualizarMensagem(Number(id), req.body);
+    res.status(200).json(mensagemAtualizada);
+  } catch (error) {
+    res.status(400).json({ erro: error.message });
+  }
+};
+
+const deletarMensagem = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await mensagemModel.deletarMensagem(Number(id));
+    res.status(204).send();
+  } catch (error) {
+    res.status(404).json({ erro: error.message });
+  }
+};
+
+module.exports = {
+  listarMensagens,
+  buscarMensagemPorId,
+  criarMensagem,
+  atualizarMensagem,
+  deletarMensagem
+};
