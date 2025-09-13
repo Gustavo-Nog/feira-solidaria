05e35033143a3a85f53ca8f44018795e2fd64ccf
Gustavo-Nog <gustavopan3g7@gmail.com>
2025-09-12 23:10:17 -0300

    refactor: melhorias no layout e navegação do Dashboard

--- frontend/src/pages/Admin/Dashboard/AdminDashboard.jsx
+++ frontend/src/pages/Admin/Dashboard/AdminDashboard.jsx
@@ -1,12 +1,51 @@
-import React from "react";
+import { useState } from 'react';
+import { FaUsers, FaBasketShopping } from 'react-icons/fa6';
+import { Link } from 'react-router-dom';
+import { FormProvider, useForm } from 'react-hook-form';
 
-function AdminDashboard() {
+import ModalAdmin from '../../../components/Admin/ModalAdmin/ModalAdmin';
+import InputField from '../../../components/Input/InputField';
+
+import usuarioServices from '../../../services/usuarioService';
+
+function Dashboard() {
+
+
+
+	const totalUsuarios = async () => {
+	}
 
 	return (
 		<>
-	
+			<div className="container h-100">
+				<div className="row h-50">
+						<div className="col-6 h5 border border-primary d-flex align-items-center justify-content-center">
+							<Link to='/usuarios' className="text-decoration-none text-dark w-100 h-100 d-flex align-items-center justify-content-center">
+									<FaUsers />&nbsp;Total de usuários
+							</Link>
+						</div>
+						<div className="col-6 h5 border border-primary d-flex align-items-center justify-content-center">
+							<Link to='/listar-produtos' className="text-decoration-none text-dark w-100 h-100 d-flex align-items-center justify-content-center">
+								<FaBasketShopping />&nbsp;Total de produtos
+							</Link>
+						</div>
+				</div>
+				<div className="row h-50">
+					<div className="col border border-primary">
+						1 de 3
+						    
+					</div>
+					<div className="col border border-primary">
+						2 de 3
+					</div>
+					<div className="col border border-primary">
+						3 de 3
+					</div>
+				</div>
+			</div>
+			
 		</>
 	);
 }
 
-export default AdminDashboard;
+export default Dashboard;
