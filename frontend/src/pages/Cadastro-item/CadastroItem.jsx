1d7b93a61ec81bb4684f2c0518fd6ecb4e9d61ea
Gustavo-Nog <gustavopan3g7@gmail.com>
2025-09-20 22:57:33 -0300

    fix: corrige tela para inputs em duas colunas e responsividade para telas menores

--- frontend/src/pages/Cadastro-item/CadastroItem.jsx
+++ frontend/src/pages/Cadastro-item/CadastroItem.jsx
@@ -1,93 +1,148 @@
 import { useForm, FormProvider } from 'react-hook-form';
 import InputField from '../../components/Input/InputField';
 import { useNavigate } from 'react-router-dom';
+import Button from '../../components/Button/Button';
+import {qualidadeOptions, statusOptions} from '../Admin/ProdutosAdmin/ProdutosAdmin.jsx';
+
 import produtoService from '../../services/produtoServices';
+import categoriaServices from '../../../services/categoriaServices';
+
 import './Cadastro-item.css';
 
 function CadastrarItem() {
+	const [produtos, setProdutos] = useState([]);
+	const [categorias, setCategorias] = useState([]);
+
+
   const methods = useForm();
   const navigate = useNavigate();
 
-   const onSubmit = async (data) => {
-    try {
-      const response = await produtoService.criarProduto(data); 
-      alert('Item cadastrado com sucesso!');
-      console.log('Resposta do backend:', response);
-      navigate('/perfil'); 
-    } catch (error) {
-      console.error('Erro ao cadastrar item:', error);
-      alert('Erro ao cadastrar item. Verifique o console.');
-    }
+	useEffect(() => {
+		const fetchCategorias = async () => {
+			try {
+				const data = await categoriaServices.listarCategorias();
+				setCategorias(data);
+			} catch (error) {
+				console.error("Erro ao buscar categorias:", error);
+			}
+		}
+		fetchCategorias();
+	}, []);
+
+	const onSubmit = async (data) => {
+		try {
+			const formData = new Formdata();
+			
+			formData.append('dados', JSON.stringify({
+				nomeProduto: data.nomeProduto,
+				descricao: data.descricao,
+				categoriaId: Number(data.categoriaId),
+				quantidade: Number(data.quantidade),
+				qualidade: data.qualidade?.toUpperCase(),
+				status: data.status?.toUpperCase()
+			}));
+
+			if (data.imagemUrl && data.imagemUrl[0]) {
+				formData.append('imagem', data.imagemUrl[0]);
+			};
+
+			const response = await produtoService.criarProduto(formData);
+			setProdutos([...produtos, response]); 
+			console.log('Resposta do backend:', response);
+			navigate('/perfil'); 
+		} catch (error) {
+			console.error('Erro ao cadastrar item:', error);
+			alert('Erro ao cadastrar item. Verifique o console.');
+		}
   };
+
+	const categoriaOptions = categorias.map(categoria => ({
+		value: categoria.id,
+		label: categoria.nome
+	}))
 
   return (
     <FormProvider {...methods}>
-      <div className="formulario-container">
-        <h2>Cadastro de Item - Feira Solidária</h2>
+      <div className="formulario-container container py-4">
+        <h2 className="text-center text-uppercase fw-bold mb-4">Cadastro de Item </h2>
+
         <form className="formulario" onSubmit={methods.handleSubmit(onSubmit)}>
+          <div className="row g-3">
+            <div className="col-md-6">
+              <div className="campo">
+                <InputField 
+									name="nomeProduto" 
+									label="Nome" 
+									required
+								/>
+              </div>
+            </div>
 
-          {/* Nome do Produto */}
-          <InputField
-            name="nome"
-            label="Nome do Produto:"
-            required
-          />
+            <div className="col-md-6">
+              <div className="campo">
+                <InputField
+									name="categoriaId"
+									label="Categoria"
+									as="select"
+									options={categoriaOptions}
+								/>
+              </div>
+            </div>
 
-          {/* Categoria */}
-          <InputField
-            as="select"
-            name="categoria"
-            label="Categoria:"
-            required
-            options={[
+            <div className="col-md-6">
+              <div className="campo">
+                <InputField name="localizacao" label="Localização (Bairro ou Cidade):" required />
+              </div>
+            </div>
 
-              { value: 'Folha', label: 'Folha' },
-              { value: 'Talo', label: 'Talo' },
-              { value: 'legume', label: 'Legume' },
-              { value: 'Outro', label: 'Outro' },
-            ]}
-          />
+            <div className="col-md-6">
+              <div className="campo">
+                <InputField
+									name="qualidade"
+									label="Qualidade"
+									as="select"
+									options={qualidadeOptions}
+								/>
+              </div>
+            </div>
 
-          {/* Localização */}
-          <InputField
-            name="localizacao"
-            label="Localização (Bairro ou Cidade):"
-            required
-          />
+            <div className="col-md-6">
+              <div className="campo">
+                <InputField 
+									name="telefone" 
+									label="Telefone:" 
+									required placeholder="(99) 99999-9999" 
+								/>
+              </div>
+            </div>
 
-          {/* Descrição */}
-          <InputField
-            as="textarea"
-            name="descricao"
-            label="Descrição:"
-            required
-            rows={3}
-          />
+            <div className="col-12">
+              <div className="campo">
+                <InputField 
+								as="textarea" 
+								name="descricao" 
+								label="Descrição:" required rows={3} />
+              </div>
+            </div>
 
-          {/* Qualidade */}
-          <InputField
-            as="select"
-            name="qualidade"
-            label="Qualidade:"
-            required
-            options={[
+            <div className="col-12 d-flex flex-column flex-sm-row justify-content-between gap-2">
+              <Button
+                type="button"
+                className="btn-danger w-100 w-sm-auto"
+                onClick={() => navigate('/perfil')}
+              >
+                Cancelar
+              </Button>
 
-              { value: 'otima', label: 'Ótima' },
-              { value: 'Boa', label: 'Boa' },
-              { value: 'regular', label: 'Regular' },
-            ]}
-          />
-          
-          {/* Telefone */}
-          <InputField
-              name="telefone"
-             label="Telefone:"
-             required="O telefone é obrigatório."
-            placeholder="(99) 99999-9999"
-            />
-          <button type="submit" className="botao">
-            Cadastrar Item
-          </button>
+              <Button
+                type="button"
+                className="btn-primary w-100 w-sm-auto"
+								onClick={methods.handleSubmit(onSubmit)}
+              >
+                Cadastrar Item
+              </Button>
+            </div>
+          </div>
         </form>
       </div>
     </FormProvider>
